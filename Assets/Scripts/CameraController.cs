62a079df5669521762250e2f7d851d5f135cfc6c
Whatar <32832231+Whatar@users.noreply.github.com>
2024-01-26 20:14:07 +0100

    stationary camera basics

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [System.Serializable]
+    private struct Target
+    {
+        public GameObject obj;
+    }
+
+    private int index = 0;
+    [SerializeField]
+    private Target[] targets;
+    [SerializeField]
+    private float time = 2f;
+
+    private bool isLerping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set the camera to the first target
+        transform.position = targets[index].obj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if space is pressed, NextTarget is called
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space pressed");
+            NextTarget();
+        }
+
+        if (isLerping)
+        {
+            if (Vector3.Distance(transform.position, targets[index].obj.transform.position) < 0.1f)
+            {
+                isLerping = false;
+            }
+            transform.position = Vector3.Lerp(transform.position, targets[index].obj.transform.position, time * Time.deltaTime);
+        }
+    }
+
+    void NextTarget()
+    {
+        // slowly lerp to the next target using transform.position
+        index++;
+        if (index >= targets.Length)
+        {
+            index = 0;
+        }
+        isLerping = true;
+    }
+
+    void PreviousTarget()
+    {
+        // slowly lerp to the previous target
+        index--;
+        if (index < 0)
+        {
+            index = targets.Length - 1;
+        }
+        isLerping = true;
+    }
+}
