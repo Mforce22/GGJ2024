70a39d7cb1fcaf24177dfdb4acc3ea59d832908b
Luca Bertini <bertini.luca00@gmail.com>
2024-01-26 23:38:38 +0100

    Created player movement and jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,17 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class CharacterController : MonoBehaviour
 {
     [SerializeField]
     private IdContainer _IdProvider;
 
+    private float MaxSpeed;
+    private float acceleration;
+    private float deceleration;
+
+    private float jumpForce;
+
+    [Header("Player Data")]
+    public PlayerDataScriptable playerData;
+
+
+    private bool isJumping;
+
+    private float currentDirection = 0;
+    private float currentSpeed = 0;
+
+    private Rigidbody rigidbody;
+    private BoxCollider collider;
 
     private GameplayInputProvider _gameplayInputProvider;
 
     private void Awake()
     {
         _gameplayInputProvider = PlayerController.Instance.GetInput<GameplayInputProvider>(_IdProvider.Id);
+
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
+
+        MaxSpeed = playerData.MaxSpeed;
+        acceleration = playerData.acceleration;
+        deceleration = playerData.deceleration;
+        jumpForce = playerData.jumpForce;
+
     }
 
     private void OnEnable()
@@ -29,12 +56,61 @@ public class CharacterController : MonoBehaviour
 
     private void JumpCharacter()
     {
-        Debug.Log("JUMP");
+        if (Physics.Raycast(transform.position, Vector3.down, collider.size.y / 2 + 0.05f))
+        {
+            //Debug.Log("JUMP");
+            rigidbody.AddForce(new Vector3(0, jumpForce, 0));
+        }
+        else
+        {
+            //Debug.Log(rigidbody.velocity.y);
+        }
+
+        //Debug.DrawRay(transform.position, Vector3.down * collider.size.y / 2);
     }
 
     private void MoveCharacter(float value)
     {
-        Debug.LogFormat("Value: {0}", value);
+        //Debug.LogFormat("Value: {0}", value);
+        currentDirection = value;
+
     }
+
+    private void Update()
+    {
+
+        // Movement
+        HandleMovement();
+
+        Vector3 movement = new Vector3(currentSpeed, 0, 0);
+
+        transform.Translate(movement * Time.deltaTime);
+        //Debug.DrawRay(transform.position, Vector3.down * collider.size.y / 2);
+    }
+
+
+    private void HandleMovement()
+    {
+        if (currentDirection != 0)
+        {
+            currentSpeed += acceleration * currentDirection;
+        }
+        else
+        {
+            currentSpeed += deceleration * -Math.Sign(currentSpeed);
+        }
+
+        if (Math.Abs(currentSpeed) < 1.5f * deceleration)
+        {
+            currentSpeed = 0;
+        }
+
+        currentSpeed = currentSpeed > MaxSpeed ? MaxSpeed : currentSpeed;
+        currentSpeed = currentSpeed < -MaxSpeed ? -MaxSpeed : currentSpeed;
+
+
+
+    }
+
 
 }
