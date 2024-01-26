ef843822c656abcaabdf22b8f9b7cddb60ef61be
Magurd <matte.guardi@gmail.com>
2024-01-26 20:29:40 +0100

    Added a game master and an event to change level

---
+++ Assets/Scripts/GameMaster/GameMaster.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : Singleton<GameMaster>, ISystem
+{
+    [SerializeField]
+    private int _Priority;
+    public int Priority { get => _Priority; }
+
+    [Header("Variables")]
+    [SerializeField]
+    private int activeLevel = 0;
+
+    [Header("Events")]
+    [SerializeField]
+    private GameEvent changeLevelEvent;
+
+
+    public void Setup()
+    {
+        SystemCoordinator.Instance.FinishSystemSetup(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int GetActiveLevel()
+    {
+        return activeLevel;
+    }
+
+    public void SetActiveLevel(int level) {  
+        activeLevel = level;
+        changeLevelEvent.Invoke();
+    }
+}
