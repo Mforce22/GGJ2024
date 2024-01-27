ca97b0f7c4b64bb971db69d0bad74ac3a8ed6b1d
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2024-01-27 16:17:27 +0100

    Menu logic fixed

---
+++ Assets/Scripts/UI/CreditsViewController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsViewController : MonoBehaviour
+{
+    public void CloseCredits()
+    {
+        Destroy(gameObject);
+    }
+}
