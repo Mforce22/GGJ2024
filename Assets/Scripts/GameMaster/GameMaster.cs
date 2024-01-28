d0dfb77936e3d93be4717f791e97e5ebac550113
Luca Bertini <bertini.luca00@gmail.com>
2024-01-28 13:41:48 +0100

    added water drops, now they kill the player

--- Assets/Scripts/GameMaster/GameMaster.cs
+++ Assets/Scripts/GameMaster/GameMaster.cs
@@ -152,6 +152,7 @@ public class GameMaster : Singleton<GameMaster>, ISystem
     void NextLevel()
     {
         camera.NextTarget();
+        changeLevelEvent.Invoke();
     }
 
     void PreviousLevel()
